af30d90b948dd5b01c2c3333ed9e77b64d03f1fd
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:25:26 +0330

    add fetch code and create context for use data az global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from "react";
 import FilterButtons from "./components/FilterButtons/FilterButtons";
+import Movies from "./components/Movies/Movies";
+
+export const moviesContext = createContext();
 
 function App() {
+    const [state, setState] = useState([]);
+    const [filter, setFilter] = useState([]);
+
+    const fetchMovie = async () => {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=f49561560a84d4dad2f6374b405a7636&language=en-US&page=1`,
+        );
+        const movies = await response.json();
+        setState(movies.results);
+        setFilter(movies.results);
+    };
+
+    useEffect(() => {
+        fetchMovie();
+    }, []);
+
+    const value = {
+        state,
+        filter,
+        setFilter,
+    };
     return (
-        <div className="app">
-            <FilterButtons />
-        </div>
+        <moviesContext.Provider value={value}>
+            <div className="app">
+                <FilterButtons />
+                <div className="image-container">
+                    <Movies />
+                </div>
+            </div>
+        </moviesContext.Provider>
     );
 }
 
