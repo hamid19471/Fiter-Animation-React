454c7c2d6a85103fc47a94b817d061710409a3ef
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:43:36 +0330

    Add code for filter afrer click and conditional syle for button

--- src/components/FilterButtons/FilterButtons.jsx
+++ src/components/FilterButtons/FilterButtons.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { moviesContext } from "../../App";
 import { filters } from "../FilterItems/FitlterItems";
 
 import "./FilterButtond.style.css";
 
 const FilterButtons = () => {
+    const [active, setActive] = useState(0);
+    const { state, setFilter } = useContext(moviesContext);
+
+    const handleActiveButton = (id) => {
+        setActive(id);
+        const filterMovies = state.filter((item) =>
+            item.genre_ids.includes(id),
+        );
+        if (id === 0) {
+            setFilter(state);
+        } else {
+            setFilter(filterMovies);
+        }
+    };
+
     return (
         <div className="container">
             <div className="filter-button">
                 {filters.map((item) => (
-                    <button key={item.id}> {item.name} </button>
+                    <button
+                        className={active === item.id ? "active" : ""}
+                        key={item.id}
+                        onClick={() => handleActiveButton(item.id)}
+                    >
+                        {" "}
+                        {item.name}{" "}
+                    </button>
                 ))}
             </div>
         </div>
