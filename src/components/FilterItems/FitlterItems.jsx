d41fd67dc3c469a280c8afbbcb727fc548549d3e
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:43:54 +0330

    Change ids base on data ids fetched

--- src/components/FilterItems/FitlterItems.jsx
+++ src/components/FilterItems/FitlterItems.jsx
@@ -1,7 +1,7 @@
 export const filters = [
     { id: 0, name: "All" },
-    { id: 1, name: "Actions" },
-    { id: 2, name: "Adventures" },
-    { id: 3, name: "Animation" },
-    { id: 4, name: "Comedy" },
+    { id: 28, name: "Actions" },
+    { id: 12, name: "Adventures" },
+    { id: 16, name: "Animation" },
+    { id: 35, name: "Comedy" },
 ];
