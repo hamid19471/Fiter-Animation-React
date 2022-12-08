80c001dbf4b9f572dbb6fb31accf063779d724ce
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:44:11 +0330

    update code for filtering

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from "react";
 import { moviesContext } from "../../App";
 import "./Movies.style.css";
 const Movies = () => {
-    const { state } = useContext(moviesContext);
+    const { filter } = useContext(moviesContext);
 
-    return state.map((item) => (
+    return filter.map((item) => (
         <div className="image-card">
             <img
                 src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
