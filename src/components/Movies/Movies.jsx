1dec82582cecb3eb62ef0778f11ca56402813e71
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:54:11 +0330

    Add motion animation for filter

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,17 +1,26 @@
 import React, { useContext } from "react";
 import { moviesContext } from "../../App";
+import { motion } from "framer-motion";
 import "./Movies.style.css";
 const Movies = () => {
     const { filter } = useContext(moviesContext);
 
     return filter.map((item) => (
-        <div className="image-card">
+        <motion.div
+            className="image-card"
+            key={item.id}
+            layout
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+        >
             <img
                 src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
                 alt={item.title}
             />
             <h2>{item.title}</h2>
-        </div>
+        </motion.div>
     ));
 };
 
