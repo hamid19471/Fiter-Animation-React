62c0714f7412e312e5a27d781f9ab1ebf1b24480
hamid19471 <hamid.19471@gmail.com>
2022-12-08 16:24:33 +0330

    develop filter items

--- src/components/FilterItems/FitlterItems.jsx
+++ src/components/FilterItems/FitlterItems.jsx
@@ -1,1 +1,7 @@
-export const filters = ["All", "Actions", "Animation", "Comedy", "Adventure"];
+export const filters = [
+    { id: 0, name: "All" },
+    { id: 1, name: "Actions" },
+    { id: 2, name: "Adventures" },
+    { id: 3, name: "Animation" },
+    { id: 4, name: "Comedy" },
+];
